2e3fb04c2dc8fd6f951a1a791e6bd3215c008796
rjawesome <aonedtop@gmail.com>
2020-05-24 02:37:10 -0700

    made password limit huge

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def create():
     global num
     num += 1
     num2 = random.randrange(99999)
-    password = str(random.randrange(99999999))
+    password = str(random.randrange(99999999999))
     sessionid = str(num)+str(num2)
     sessions[sessionid] = {"session": sessionid, "password": password, "data": [], "messages": []}
     @socketio.on('drawing_'+sessionid+':'+password)
